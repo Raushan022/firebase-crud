6bff4768650e7a566a0c8b6f4a79a68db960885a
raushan <kraushan022@gmail.com>
2025-11-27 20:58:07 +0530

    read data and actions ui

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,20 @@ const App = () => {
   };
   const [employees, setEmployees] = useState(employeeModel);
   const [isEmpty, setIsEmpty] = useState(false);
+  const [employeeData, setEmployeeData] = useState([]);
 
   useEffect(() => {
     const req = async () => {
       const allEmployeesData = await getDocs(collection(db, "employeesData"));
       setIsEmpty(allEmployeesData.empty);
+
+      //read data that we received from db
+      let tmp = [];
+      allEmployeesData.forEach((doc) => {
+        const document = doc.data();
+        tmp.push(document);
+      });
+      setEmployeeData(tmp);
     };
 
     req();
@@ -41,6 +50,7 @@ const App = () => {
       //and since this is server task so it will take time hence we will make async function and await it
       await addDoc(collection(db, "employeesData"), employees);
       setIsEmpty(false);
+      // setEmployeeData(...employeeData, employees);
 
       new Swal({
         icon: "success",
@@ -63,8 +73,8 @@ const App = () => {
       <h1 className="text-5xl font-bold">
         Firebase <span className="text-indigo-600">CRUD</span>
       </h1>
-      <div className="grid grid-cols-2 w-8/12 gap-16">
-        <div>
+      <div className="flex w-10/12 gap-16">
+        <div className="w-[400px]">
           <form className="space-y-6" onSubmit={createEmployee}>
             <div className="flex flex-col">
               <label className="font-semibold text-lg mb-2">
@@ -111,13 +121,47 @@ const App = () => {
             </button>
           </form>
         </div>
-        <div className="bg-rose-400">
+        <div className=" flex-1">
           {isEmpty && (
             <div className="flex flex-col items-center">
               <i className="ri-u-disk-line text-3xl text-gray-600"></i>
               <h1 className="text-3xl text-gray-600 ">Empty</h1>
             </div>
           )}
+
+          <h1 className="text-2xl font-semibold">Employees</h1>
+          <table className="w-full mt-8">
+            <thead>
+              <tr className="bg-rose-600 text-white text-left">
+                <th className="py-2 pl-2">S/No</th>
+                <th>Employee Name</th>
+                <th>Salary</th>
+                <th>Joining Date</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employeeData.map((item, index) => (
+                <tr key={index} className="border-b border-gray-300">
+                  <td className="pl-2 py-2">{index + 1}</td>
+                  <td>{item.employeeName}</td>
+                  <td>{item.salary}</td>
+                  <td>{item.joiningDate}</td>
+                  <td>
+                    <div className="space-x-2">
+                      <button className="w-8 h-8 bg-indigo-600 text-white rounded-full">
+                        <i className="ri-file-edit-line"></i>
+                      </button>
+
+                      <button className="w-8 h-8 bg-pink-600 text-white rounded-full">
+                        <i className="ri-delete-bin-6-line"></i>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
